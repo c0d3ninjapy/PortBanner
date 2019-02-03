e133628fcf58139db0074d7b11ca8f4e247ddc00
c0d3ninjapy <43689157+c0d3ninjapy@users.noreply.github.com>
2019-02-03 01:57:44 -0500

    Update portbanner.py

--- portbanner.py
+++ portbanner.py
@@ -1,7 +1,13 @@
 import socket, sys
 
+# Coded by c0deninja
+# Discord: http://discord.gg/JV6pu5q
+# Connects to a specific port and prints out the banner.
+# This script is useful for pen testers to check the version of the service running on that port.
+# EX: python portbanner.py IP PORT
+
 if len(sys.argv) !=3:
-	print ("Usage ./portbanner.py <host> <port>")
+	print ("Usage portbanner.py <host> <port>")
 	sys.exit(1)
 
 host = sys.argv[1]
